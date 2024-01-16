90e46911b4e7d3b088d90e233a92aa33bd99d1d8
Kaustubh <pro.vaidya4413@gmail.com>
2024-01-16 10:45:09 +0530

    Initial setup and project start

---
+++ lib/view/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_contact_picker/flutter_native_contact_picker.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
